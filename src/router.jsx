6b864efcc4aa6eee7fa0721a300ced048af025d1
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-09 16:02:23 +0000

    feat: add leaderboard loader to fetch all score data

--- src/router.jsx
+++ src/router.jsx
@@ -18,7 +18,15 @@ const routes = [
             },
             {
                 path: "/leaderboard",
-                element: <LeaderboardPage />
+                element: <LeaderboardPage />,
+                loader: async () => {
+                    const response = await fetch("/api/score/leaderboard");
+                    if (response.status === 500) {
+                        throw new Response("Internal Server Error", {status: 500});
+                    }
+                    
+                    return response.json();
+                }
             }
         ]
     }
