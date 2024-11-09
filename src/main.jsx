dd5a28b2ca60b45a45706f3e67c82b9c72f131d4
Muhsina Kamal <muhsina_k@hotmail.co.uk>
2024-11-09 13:41:47 +0000

    feat: add createBrowserRouter to enable support of react router data APIs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { UserProvider } from './contexts/UserContext.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routes from './router.jsx';
 import './index.css'
-import App from './App.jsx'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <RouterProvider router={router} />
     </StrictMode>
 )
